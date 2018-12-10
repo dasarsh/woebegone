33eca377f90d5ac17359bed08c5a3428f5a914f8
dasarsh <dasarsh@gmail.com>
2018-12-10 23:21:28 +1300

    Injected quick math service into screen component

--- src/app/calc-screen/calc-screen.component.ts
+++ src/app/calc-screen/calc-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuickMathService } from '../quick-math.service';
 
 @Component({
   selector: 'app-calc-screen',
@@ -9,7 +10,7 @@ export class CalcScreenComponent implements OnInit {
 
   public value: string;
 
-  constructor() { 
+  constructor(private quickMathService: QuickMathService) { 
     this.value = "10";
   }
 
