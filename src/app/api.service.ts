8fccc20fec759e6ebbf47c6a4da58d430dfaf2d6
dasarsh <dasarsh@gmail.com>
2018-12-12 19:41:14 +1300

    Added API GET to example placeholder

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getAll<T>(): void {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  }
 }
--- src/app/calc-nav/calc-nav.component.ts
+++ src/app/calc-nav/calc-nav.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { QuickMathService } from '../quick-math.service';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-calc-nav',
@@ -15,8 +16,9 @@ export class CalcNavComponent {
     .pipe(
       map(result => result.matches)
     );
-  constructor(private breakpointObserver: BreakpointObserver, private quickMathService: QuickMathService) {}
+  constructor(private breakpointObserver: BreakpointObserver, private quickMathService: QuickMathService, private apiService: ApiService) {}
 
   call(): void{
+    var result = this.apiService.getAll();
   }
 }
